a29ffbce5b3fffd2094a5b8d86a643bb0c8508e5
Vincent Le <vincentle.fh@gmail.com>
2023-06-18 18:45:11 +0300

    Fixed api for vercel

--- server/src/app.js
+++ server/src/app.js
@@ -5,8 +5,8 @@ const cors = require( "cors" )
 const middleware = require( "./utils/middleware" )
 const logger = require( "./utils/logger" )
 const mongoose = require( "mongoose" )
-const todoRouter = require( "./controllers/todos" )
-const projectRouter = require( "./controllers/projects" )
+const todoRouter = require( "../controllers/todos" )
+const projectRouter = require( "../controllers/projects" )
 
 logger.info( "Connecting to", config.MONGODB_URI )
 
@@ -23,7 +23,7 @@ app.use( "/", todoRouter )
 app.use( "/", projectRouter )
 
 if (process.env.NODE_ENV === "test") {  
-    const testingRouter = require("./controllers/testing")  
+    const testingRouter = require("../controllers/testing")  
     app.use("/api/testing", testingRouter
 )}
 
